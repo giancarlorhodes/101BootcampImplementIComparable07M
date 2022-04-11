0cf6dee3f8946e7e338b82a1e87589b78b8c8e31
Giancarlo Rhodes <giancarlo.rhodes@onshoreoutsourcing.com>
2022-04-11 08:56:19 -0500

    Add project files.

---
+++ 101BootcampImplementIComparable07M/House.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _101BootcampImplementIComparable07M
+{
+    internal class House : IComparable<House>
+    {
+        public int SquareFootage { get; set; }
+        public string Description { get; set; }
+
+        public House(int sq) { this.SquareFootage = sq; }
+
+        public House(int sq, string d) { this.SquareFootage = sq; this.Description = d; }
+
+        public int CompareTo(House h)
+        {
+           
+            if (h.SquareFootage > this.SquareFootage)
+                return -1;
+            else if (h.SquareFootage < this.SquareFootage)  
+                return 1;
+            else
+                 return 0;
+                  
+        }
+    }
+
+
+    
+}
---
+++ 101BootcampImplementIComparable07M/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _101BootcampImplementIComparable07M
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            House h1 = new House(15500, "6743 Daveport Drive, Smallville, Ohio 58390");
+    
+            House h2 = new House(1200, "1101 Willocreek Lane, Columbia, MO 65203");
+
+            House h3 = new House(4500, "1123 Willocreek Lane, Columbia, MO 65203");
+
+            House h4 = new House(15500, "14566 Daveport Drive, Smallville, Ohio 58390");
+
+            House h5 = new House(4534, "345 Daveport Drive, Smallville, Ohio 58390");
+
+            House[] houses = new House[5];
+            houses[0] = h1;
+            houses[1] = h2;     
+            houses[2] = h3;
+            houses[3] = h4;
+            houses[4] = h5;
+
+
+            // using the Icomparable interface
+            Array.Sort(houses);
+            Array.ForEach(houses, h => Console.WriteLine($"House description and footage: {h.Description}, {h.SquareFootage}"));
+
+            
+            
+
+
+        }
+    }
+}
